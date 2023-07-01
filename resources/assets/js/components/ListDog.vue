294817ae8b41da5e7bfdf447a58fbcb41002aab7
nicolopezdelerma <nicolopezdelerma@gmail.com>
2023-07-01 12:44:06 +0200

    remove modal

--- resources/assets/js/components/ListDog.vue
+++ resources/assets/js/components/ListDog.vue
@@ -43,57 +43,6 @@
             </div>
           </div>
         </div>
-
-        <!-- Modal -->
-        <div class="modal" :style="{ display: mostrarModal ? 'block' : 'none' }">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Nuevo Perro</h5>
-                <button type="button" class="close" @click="cerrarModal">
-                  <span>&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <form @submit="añadirPerro" :action="formAction" method="PUT">
-                  <input type="hidden" name="_method" value="PUT">
-                  <input type="hidden" name="_token" :value="csrfToken">
-
-                  <div class="form-group">
-                    <label for="race">Raza:</label>
-                    <input type="text" name="race" class="form-control" v-model="perro.race" required>
-                  </div>
-
-                  <div class="form-group">
-                    <label for="name">Nombre:</label>
-                    <input type="text" name="name" class="form-control" v-model="perro.name" required>
-                  </div>
-
-                  <div class="form-group">
-                    <label for="size">Tamaño:</label>
-                    <select name="size" class="form-control" v-model="perro.size" required>
-                      <option value="">Selecciona un tamaño</option>
-                      <option v-for="size in sizeOptions" :value="size.value">{{ size.label }}</option>
-                    </select>
-                  </div>
-
-                  <div class="form-group">
-                    <div class="flex">
-                      <label for="color">Color:</label>
-                      <div class="color-indicator" :class="colorSelected"></div>
-                    </div>
-                    <select name="color" class="form-control" v-model="perro.hair_color" @change="updateColorIndicator" required>
-                      <option value="">Selecciona un color</option>
-                      <option v-for="color in colors" :value="color">{{ color }}</option>
-                    </select>
-                  </div>
-
-                  <button type="submit" class="btn btn-success">Añadir</button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </template>
 
@@ -102,7 +51,6 @@
       props: ['perros'],
       data() {
         return {
-          mostrarModal: false,
           csrfToken: '',
           mostrarNotificacion: false,
           tipoNotificacion: '',
@@ -179,59 +127,6 @@
             });
         },
 
-        abrirModalNuevoPerro() {
-          console.log('abrir modal');
-          this.mostrarModal = true;
-        },
-
-        cerrarModal() {
-          this.mostrarModal = false;
-        },
-
-        añadirPerro(event) {
-          event.preventDefault();
-          console.log(event);
-          // Realizar la solicitud POST para agregar el perro
-          fetch('/perros', {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json',
-              'X-CSRF-TOKEN': this.csrfToken,
-            },
-            body: JSON.stringify(this.perro),
-          })
-            .then(response => response.json())
-            .then(data => {
-              // Lógica para manejar la respuesta del servidor
-              console.log(data);
-
-              // Mostrar la notificación de éxito
-              this.mensajeNotificacion = data.message;
-              this.mostrarNotificacion = true;
-              this.tipoNotificacion = 'success';
-
-              // Agregar el nuevo perro a la lista
-              this.perros.push(data.perro);
-
-              // Ocultar el modal y limpiar los datos del perro
-              this.cerrarModal();
-              this.perro = {
-                race: '',
-                name: '',
-                size: '',
-                hair_color: '',
-              };
-            })
-            .catch(error => {
-              // Lógica para manejar el error de la solicitud
-              console.error(error);
-
-              // Mostrar la notificación de error
-              this.mensajeNotificacion = 'Hubo un error al añadir el perro.';
-              this.mostrarNotificacion = true;
-              this.tipoNotificacion = 'error';
-            });
-        },
 
         updateColorIndicator() {
           // Realizar acciones adicionales según sea necesario
