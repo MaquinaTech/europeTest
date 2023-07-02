a35b0423988cf2d415b25b151e8e600e11d74e38
nicolopezdelerma <nicolopezdelerma@gmail.com>
2023-07-02 14:40:34 +0200

    fix logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,18 +61,19 @@
                                 
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
-                                        <a href="{{ url('/logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
+                                        
                                     </li>
                                 </ul>
                             </li>
+                            <a class="dropdown-toggle" href="{{ url('/logout') }}"
+                                onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+
+                            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
                             
                         @endif
                     </ul>
